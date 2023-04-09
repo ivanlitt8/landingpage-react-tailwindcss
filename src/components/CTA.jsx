20ce432576f8ab51fe614628c675ad0627dd981f
Iván <ivanlitt8@gmail.com>
2023-04-09 17:06:55 -0300

    Terminado

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import styles from "../styles";
+import Button from "./Button";
 
-const CTA = () => {
-    return (
-        <div>CTA</div>
-    )
-}
+const CTA = () => (
+    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+        <div className="flex-1 flex flex-col">
+            <h2 className={styles.heading2}>Try our service now!</h2>
+            <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With our highly skilled team and advanced technology, we can efficiently solve your problems and provide you with an exceptional service experience.</p>
+        </div>
+        <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+            <Button />
+        </div>
+    </section>
+)
+
 
 export default CTA
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { card } from "../assets";
+import styles, { layout } from "../styles";
+import Button from "./Button";
 
-const CardDeal = () => {
-    return (
-        <div>CardDeal</div>
-    )
-}
+const CardDeal = () => (
+    <section id="cardDeal" className={layout.section}>
+        <div className={layout.sectionInfo}>
+            <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hidden" /> in few easy steps.</h2>
+            <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+                Download our app, enter your financial information and preferences, and our advanced algorithm will recommend the best options for you. Manage your credit cards, make payments, and track your expenses from your phone. Download our application and take control of your finances from anywhere.
+            </p>
+            <Button styles="mt-10" />
+        </div>
+        <div className={layout.sectionImg}>
+            <img src={card} alt="card" className="w-[100%] h-[100%]" />
+        </div>
+    </section>
+)
 
 export default CardDeal
